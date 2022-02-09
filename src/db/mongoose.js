286110f361df3b5bc5c77d4b6982b77fabadcfb3
EXECUTIVEGS-6 <EXECUTIVEGS-6@fivesdigital.com>
2022-02-09 14:59:20 +0530

    Creating Test Cases

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,5 +1,3 @@
 const mongoose = require('mongoose');
-const { CONNECTION_URL, DB_PORT, DB_NAME } = process.env;
-mongoose.connect(`${CONNECTION_URL}:${DB_PORT}/${DB_NAME}`, {
-    useNewUrlParser: true
-});
+
+mongoose.connect(`${process.env.CONNECTION_URL}/${process.env.DB_NAME}`);
