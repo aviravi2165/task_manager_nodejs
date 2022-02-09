4148ff8a9fec657c215f9aee5e3f865aeee93c97
aviravi2165 <avi.ravi2165@gmail.com>
2022-02-09 20:47:57 +0530

    Testing

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const mongoose = require("mongoose");
 mongoose.connect(
   `${process.env.CONNECTION_URL}/${process.env.DB_NAME}`,
@@ -7,8 +6,3 @@ mongoose.connect(
     useCreateIndex: true,
   }
 );
-=======
-const mongoose = require('mongoose');
-
-mongoose.connect(`${process.env.CONNECTION_URL}/${process.env.DB_NAME}`);
->>>>>>> e3ac0927cc57b7b743756c022ec6ca8b15bcef75
